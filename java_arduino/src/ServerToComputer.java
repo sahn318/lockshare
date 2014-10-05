317d022a6199b378d1a28325d0123e71711bc2f2
Alex Choi <rlrrlrll4@gmail.com>
2014-10-05 09:35:26 -0400

    java_arduino

---
+++ java_arduino/src/ServerToComputer.java
@@ -0,0 +1,61 @@
+import java.awt.*;
+
+import javax.swing.*;
+
+import java.net.*;
+import java.io.*;
+import java.net.URLConnection;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Enumeration;
+
+
+public class ServerToComputer{
+    public static void main(String [] args) throws Exception
+    {
+        for(int access = 1; access >= 1; access++)
+        {   
+            Thread.sleep(1000);
+            URL link = new URL("http://locksh.herokuapp.com/java");
+            URLConnection con = null;
+            con = link.openConnection();
+            String line;
+            int server = 0;
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            try{
+                while((line = br.readLine()) != null)
+                {
+                    server = Integer.parseInt(line);
+                    server--;
+                    
+                    String timeStamp = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
+
+                    if(server == 1){
+                    	System.out.println(timeStamp + ": " + "Door 1 Unlocked");
+                    }else{
+                    	System.out.println(timeStamp + ": " + "Door 1 Locked");
+                    }
+                }
+            }catch(IOException e)
+            {
+                e.printStackTrace();
+            }finally{
+                if(br != null){
+                    try{ 
+                        br.close();
+                    }catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+            
+            if(access == 1000)
+                access = 1;
+        }        
+           
+           
+    }
+}
